a2357f4d52c3674c2ff4f19be640eb91c4b43664
Mark Cawili <mark.cawili@gmail.com>
2024-06-06 16:01:39 -0600

    Created Timer javascript file and added foundational CSS

---
+++ src/components/Timer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {useState} from "react";
+
+export default function Timer() {
+
+    return (
+        <div className="pomodoro">
+            <div className="timer">25:00</div>
+        </div>
+    )
+}
