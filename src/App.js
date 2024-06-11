2a8defd83b84897c04562c6bafbe8249efd43fb0
Mark Cawili <mark.cawili@gmail.com>
2024-06-10 21:18:25 -0600

    Implemented a simple To Do List

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Timer from "./components/Timer";
+import List from "./components/List";
 
 function App() {
   return (
       <div className="App">
           <h1>Tomato Groove</h1>
           <Timer />
+          <List />
       </div>
   );
 }
---
+++ src/components/List.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import {useState} from "react";
+
+export default function ToDoList() {
+
+    const [todos, setTodos] = useState(["test"]);
+    const [inputVal, setInputVal] = useState('');
+
+    const handleInputChange = (e) => {
+        setInputVal(e.target.value);
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        /*Sets To Do List with what is currently in the list plus input value*/
+        setTodos((todo) => [...todo, inputVal]);
+        /*Resets  the input value*/
+        setInputVal('');
+    };
+
+    return (
+        <div className="list-div">
+            <h3>To Do List</h3>
+            <form onSubmit={handleSubmit}>
+                <input
+                    placeholder="Enter Todo"
+                    type="text"
+                    name="task-entry"
+                    value={inputVal}
+                    onChange={handleInputChange}
+                />
+            </form>
+            <ul>
+                {/*Every To-Do is mapped as a list*/}
+                {todos.map((todo) => (
+                    <div className="list">
+                        <button type="checkbox"></button>
+                        <li key={todo}>{todo}</li>
+                    </div>
+                ))}
+            </ul>
+        </div>
+    )
+
+}
