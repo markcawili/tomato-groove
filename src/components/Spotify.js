38239c9f3b568d89603f17e17bebc468c514fd50
Mark Cawili <mark.cawili@gmail.com>
2024-06-13 21:46:04 -0600

    Updated Spotify.js to store token to local storage

--- src/components/Spotify.js
+++ src/components/Spotify.js
@@ -1,10 +1,30 @@
 import React from 'react';
+import {useEffect, useState} from "react";
 
 export default function Spotify() {
     const CLIENT_ID = "4086684ccd334080b7243a8eba2b507c";
     const REDIRECT_URI = "http://localhost:3000/";
     const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
     const RESPONSE_TYPE = "token";
+
+    const [token, setToken] = useState("")
+
+    useEffect(() => {
+        /*From url*/
+        const hash = window.location.hash;
+        /*Get token from local storage*/
+        let token = window.localStorage.getItem("token");
+
+        /*If there is no token and there is a hash, get the token from url*/
+        if (!token && hash) {
+            token = hash.substring(1).split("&").find(elem => elem.startsWith("access_token")).split("=")[1];
+
+            /*sets the token*/
+            window.location.hash = "";
+            window.localStorage.setItem("token", token);
+            setToken(token);
+        }
+    }, []);
 
     return (
         <button>
