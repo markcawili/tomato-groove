7a79a570c330aff9e55f957991b6f7ffa76abb7e
Mark Cawili <mark.cawili@gmail.com>
2024-06-17 15:11:53 -0600

    Setup of the Spotify object from SpotifyWebAPI
    Use of SpotifyWebAPI for ease of API calls

--- src/App.js
+++ src/App.js
@@ -3,8 +3,13 @@ import List from "./components/List";
 import Login from "./components/Login";
 import React, {useEffect, useState} from "react";
 import {getTokenFromUrl} from "./spotify";
+import SpotifyWebApi from "spotify-web-api-js";
 
 function App() {
+
+    //Will be master object for our interactions with our project and the API
+    const spotify = new SpotifyWebApi();
+
     const [token, setToken] = useState(null);
 
     useEffect(() => {
@@ -17,6 +22,13 @@ function App() {
         //Use of the _ as a standard to refer to the temporary variable
         if (_token) {
             setToken(_token);
+
+            spotify.setAccessToken(_token);
+            //Gets the user account and returns a Promise
+            //If everything goes well, returns a user
+            spotify.getMe().then(user => {
+                console.log('I have a user', user);
+            });
         }
 
         console.log("I have a token", token);
