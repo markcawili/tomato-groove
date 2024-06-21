ab66fb896d207a2087bd840a6ad7a872db21dd8e
Mark Cawili <mark.cawili@gmail.com>
2024-06-20 18:16:03 -0600

    Added basic footer styling and separation of components

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,29 @@
 import React from 'react';
-import '../styles/Footer.css'
+import '../styles/Footer.css';
+import PlayCircleOutlineIcon from '@mui/icons-material/PlayCircleOutline';
+import SkipPreviousIcon from '@mui/icons-material/SkipPrevious';
+import SkipNextIcon from '@mui/icons-material/SkipNext';
+import ShuffleIcon from '@mui/icons-material/Shuffle';
+import RepeatIcon from '@mui/icons-material/Repeat';
 
 function Footer() {
     return (
         <div className="footer">
-            <h2>I am the Footer</h2>
+            <div className="footer-left">
+                <p>Album and song details</p>
+            </div>
+
+            <div className="footer-center">
+                <ShuffleIcon className="footer-green"></ShuffleIcon>
+                <SkipPreviousIcon className="footer-icon"></SkipPreviousIcon>
+                <PlayCircleOutlineIcon fontSize="large" className="footer-icon"></PlayCircleOutlineIcon>
+                <SkipNextIcon className="footer-icon"></SkipNextIcon>
+                <RepeatIcon className="footer-green"></RepeatIcon>
+            </div>
+
+            <div className="footer-right">
+                <p>Volume controls</p>
+            </div>
         </div>
     )
 }
