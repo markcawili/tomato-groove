3d7b7a82812622ddb16459f58c5127aa99ac40aa
Mark Cawili <mark.cawili@gmail.com>
2024-07-01 13:40:09 -0600

    Implementation of List closing animation as well as refactor of class names for animation

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,8 +1,7 @@
 import '../styles/Body.css'
 import {useDataLayerValue} from "./DataLayer";
 import SongRow from "./SongRow";
-import ArrowRightAltIcon from '@mui/icons-material/ArrowRightAlt';
-function Body({spotify}) {
+import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';function Body({spotify}) {
 
     const [{likedSongs}] = useDataLayerValue();
 
@@ -11,8 +10,8 @@ function Body({spotify}) {
     function close() {
         let div = document.getElementsByClassName("utility-body");
 
-        div[0].classList.remove("open-transition");
-        div[0].classList.add("close-transition");
+        div[0].classList.remove("open-transition-body");
+        div[0].classList.add("close-transition-body");
 
         /* TODO: need to add an open button on close */
     }
@@ -20,15 +19,15 @@ function Body({spotify}) {
     function open() {
         let div = document.getElementsByClassName("utility-body");
 
-        div[0].classList.remove("close-transition");
-        div[0].classList.add("open-transition");
+        div[0].classList.remove("close-transition-body");
+        div[0].classList.add("open-transition-body");
     }
 
     return (
         <div className="utility-body">
             <div className="utility-body-header">
                 <h1>Liked songs</h1>
-                <ArrowRightAltIcon className="close" fontSize="large" onClick={close}></ArrowRightAltIcon>
+                <KeyboardArrowRightIcon className="close-player" fontSize="large" onClick={close} />
             </div>
             <div className="song">
                 {likedSongs.items?.map((item) => (
--- src/components/List.js
+++ src/components/List.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import {useState} from "react";
 import '../styles/List.css'
-
-export default function ToDoList() {
+import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';export default function ToDoList() {
 
     const [todos, setTodos] = useState([]);
     const [inputVal, setInputVal] = useState('');
@@ -19,18 +18,39 @@ export default function ToDoList() {
         setInputVal('');
     };
 
+    function closeList() {
+        let div = document.getElementsByClassName("list-div");
+
+        div[0].classList.remove("open-transition-list");
+        div[0].classList.add("close-transition-list");
+
+        /* TODO: need to add an open button on close */
+    }
+
+    function open() {
+        let div = document.getElementsByClassName("list-div");
+
+        div[0].classList.remove("close-transition-list");
+        div[0].classList.add("open-transition-list");
+    }
+
     return (
         <div className="list-div">
-            <h3>To Do List</h3>
-            <form onSubmit={handleSubmit}>
-                <input
-                    placeholder="Enter Todo"
-                    type="text"
-                    name="task-entry"
-                    value={inputVal}
-                    onChange={handleInputChange}
-                />
-            </form>
+            <div className="list-div-header">
+                <div>
+                <h3>To Do List</h3>
+                    <form onSubmit={handleSubmit}>
+                        <input
+                            placeholder="Enter Todo"
+                            type="text"
+                            name="task-entry"
+                            value={inputVal}
+                            onChange={handleInputChange}
+                        />
+                    </form>
+                </div>
+                <KeyboardArrowLeftIcon className="close-list" fontSize="large" onClick={closeList} ></KeyboardArrowLeftIcon>
+            </div>
             <div>
                 <ul>
                     {/*Every To-Do is mapped as a list*/}
@@ -45,6 +65,6 @@ export default function ToDoList() {
         </div>
     )
 
-    /* TODO: The checkbox dots are getting squishes */
+    /* TODO: The checkbox dots are getting squished */
 
 }
